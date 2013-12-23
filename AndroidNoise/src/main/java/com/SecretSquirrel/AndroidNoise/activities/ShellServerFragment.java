dffec9501c785933b5acffc070ef031bcdfd1c3c
Bill Swanson <bswanson58@gmail.com>
2013-12-23 10:36:20 -0600

    Added a main activity support a navigation drawer structure for the app with skeleton fragments.

---
+++ AndroidNoise/src/main/java/com/SecretSquirrel/AndroidNoise/activities/ShellServerFragment.java
@@ -0,0 +1,8 @@
+package com.SecretSquirrel.AndroidNoise.activities;
+
+// Secret Squirrel Software - Created by bswanson on 12/23/13.
+
+import android.support.v4.app.Fragment;
+
+public class ShellServerFragment extends Fragment {
+}
