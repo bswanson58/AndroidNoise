dd181e079e2b73d4f7976568e4360b0a00347b95
Bill Swanson <bswanson58@gmail.com>
2014-01-02 13:19:10 -0600

    Added the ThreadExecutor utility class.

---
+++ AndroidNoise/src/main/java/com/SecretSquirrel/AndroidNoise/support/ThreadExecutor.java
@@ -0,0 +1,53 @@
+package com.SecretSquirrel.AndroidNoise.support;
+
+// Secret Squirrel Software - Created by bswanson on 1/2/14.
+
+import android.util.Log;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import android.os.Process;
+
+/**
+ * ThreadPoolExecutor to reduce resources when running threads. Starts with a
+ * pool size of 4 and can grow to 50. Uses java.util.Concurrency cleverness to
+ * provide the quality pooling and thread safety.
+ */
+public class ThreadExecutor {
+	private static final String TAG = ThreadExecutor.class.toString();
+	private static final int CORE_POOL_SIZE = 5;
+	private static final int MAXIMUM_POOL_SIZE = 50;
+	private static final int KEEP_ALIVE = 10;
+	private static final BlockingQueue<Runnable> sWorkQueue = new LinkedBlockingQueue<Runnable>(MAXIMUM_POOL_SIZE);
+
+	private static final ThreadFactory sThreadFactory = new ThreadFactory() {
+		private final AtomicInteger mCount = new AtomicInteger(1);
+
+		public Thread newThread( Runnable r ) {
+			final String threadName = "ThreadExecutor #" + mCount.getAndIncrement();
+
+			Log.d( TAG, String.format( "Creating Thread: %s", threadName ) );
+
+			final Thread thread = new Thread( r, threadName );
+			Process.setThreadPriority( Process.THREAD_PRIORITY_BACKGROUND );
+
+			return( thread );
+		}
+	};
+
+	private static final ThreadPoolExecutor sExecutor = new ThreadPoolExecutor(
+			CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE, TimeUnit.SECONDS, sWorkQueue, sThreadFactory );
+
+	public static void runTask( Runnable task ) {
+		Log.d( TAG, String.format( "Running ThreadExecutor Task = %d", sWorkQueue.size()));
+
+		sExecutor.execute( task );
+	}
+}
+
+
