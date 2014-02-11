80055a57bbfef33c77aa36cea8d44dd6a16b7581
Bill Swanson <bswanson58@gmail.com>
2014-02-11 10:31:18 -0600

    Refactored the AlbumListFragment using Butter Knife.

--- AndroidNoise/src/main/java/com/SecretSquirrel/AndroidNoise/activities/AlbumListFragment.java
+++ AndroidNoise/src/main/java/com/SecretSquirrel/AndroidNoise/activities/AlbumListFragment.java
@@ -4,7 +4,6 @@ package com.SecretSquirrel.AndroidNoise.activities;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.Parcelable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -46,6 +45,9 @@ import java.util.Comparator;
 
 import javax.inject.Inject;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import de.greenrobot.event.EventBus;
 
 public class AlbumListFragment extends Fragment
@@ -58,19 +60,21 @@ public class AlbumListFragment extends Fragment
 	private static final String     FILTER_TEXT = "albumListFilterText";
 	private static final String     FILTER_DISPLAYED = "albumListFilterDisplayed";
 
-	private ServiceResultReceiver   mReceiver;
 	private long                    mCurrentArtist;
 	private ArrayList<Album>        mAlbumList;
-	private ListView                mAlbumListView;
 	private Parcelable              mAlbumListViewState;
 	private AlbumAdapter            mAlbumListAdapter;
-	private TextView                mAlbumCount;
-	private ButtonEditText          mFilterEditText;
 	private String                  mFilterText;
-	private View                    mFilterPanel;
 	private boolean                 mFilterPanelDisplayed;
 
+	@Inject EventBus                mEventBus;
 	@Inject	INoiseData              mNoiseData;
+	@Inject ServiceResultReceiver   mReceiver;
+
+	@InjectView( R.id.al_album_list_view )	ListView        mAlbumListView;
+	@InjectView( R.id.al_list_count )   	TextView        mAlbumCount;
+	@InjectView( R.id.al_filter_panel ) 	View            mFilterPanel;
+	@InjectView( R.id.al_album_filter )	    ButtonEditText  mFilterEditText;
 
 	public static AlbumListFragment newInstance( long artistId ) {
 		AlbumListFragment   fragment = new AlbumListFragment();
@@ -99,16 +103,27 @@ public class AlbumListFragment extends Fragment
 			mAlbumListViewState = savedInstanceState.getParcelable( LIST_STATE );
 			mFilterText = savedInstanceState.getString( FILTER_TEXT );
 			mFilterPanelDisplayed = savedInstanceState.getBoolean( FILTER_DISPLAYED );
+			mCurrentArtist = savedInstanceState.getLong( ARTIST_KEY );
 		}
+		else {
+			Bundle  args = getArguments();
+
+			if( args != null ) {
+				mCurrentArtist = args.getLong( ARTIST_KEY, Constants.NULL_ID );
+			}
+		}
+
 		if( mAlbumList == null ) {
 			mAlbumList = new ArrayList<Album>();
 		}
 
-		mAlbumListAdapter = new AlbumAdapter( getActivity(), mAlbumList );
-		mAlbumListAdapter.setListWatcher( this );
+		if( mCurrentArtist == Constants.NULL_ID ) {
+			if( Constants.LOG_ERROR ) {
+				Log.e( TAG, "The current artist could not be determined." );
+			}
+		}
 
-		mReceiver = new ServiceResultReceiver( new Handler());
-		mReceiver.setReceiver( this );
+		mAlbumListAdapter = new AlbumAdapter( getActivity(), mAlbumList );
 	}
 
 	@Override
@@ -116,7 +131,7 @@ public class AlbumListFragment extends Fragment
 		View    myView = inflater.inflate( R.layout.fragment_album_list, container, false );
 
 		if( myView != null ) {
-			mAlbumListView = (ListView)myView.findViewById( R.id.al_album_list_view );
+			ButterKnife.inject( this, myView );
 
 			mAlbumListView.setAdapter( mAlbumListAdapter );
 			mAlbumListView.setOnItemClickListener( new AdapterView.OnItemClickListener() {
@@ -125,12 +140,11 @@ public class AlbumListFragment extends Fragment
 					Album album = mAlbumListAdapter.getItemAtPosition( i );
 
 					if( album != null ) {
-						EventBus.getDefault().post( new EventAlbumSelected( album ));
+						mEventBus.post( new EventAlbumSelected( album ) );
 					}
 				}
 			} );
 
-			mFilterEditText = (ButtonEditText)myView.findViewById( R.id.al_album_filter );
 			mFilterEditText.addTextChangedListener( new TextWatcher() {
 				@Override
 				public void onTextChanged( CharSequence charSequence, int i, int i2, int i3 ) {
@@ -149,44 +163,48 @@ public class AlbumListFragment extends Fragment
 				}
 			} );
 
-
 			if(!TextUtils.isEmpty( mFilterText )) {
 				mFilterEditText.setText( mFilterText );
 			}
 
-			mAlbumCount = (TextView)myView.findViewById( R.id.al_list_count );
-
-			mFilterPanel = myView.findViewById( R.id.al_filter_panel );
 			displayFilterPanel( mFilterPanelDisplayed, false );
 
-			if( savedInstanceState != null ) {
-				if( mAlbumListViewState != null ) {
-					mAlbumListView.onRestoreInstanceState( mAlbumListViewState );
-				}
-
-				mCurrentArtist = savedInstanceState.getLong( ARTIST_KEY );
-			}
-			else {
-				Bundle  args = getArguments();
-
-				if( args != null ) {
-					mCurrentArtist = args.getLong( ARTIST_KEY, Constants.NULL_ID );
-				}
-			}
-		}
-
-		if( mCurrentArtist != Constants.NULL_ID ) {
-			if( mAlbumList.size() == 0 ) {
-				mNoiseData.GetAlbumList( mCurrentArtist, mReceiver );
-			}
-		}
-		else {
-			if( Constants.LOG_ERROR ) {
-				Log.e( TAG, "The current artist could not be determined." );
+			if(( savedInstanceState != null ) &&
+			   ( mAlbumListViewState != null )) {
+				mAlbumListView.onRestoreInstanceState( mAlbumListViewState );
 			}
 		}
 
 		return( myView );
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		mAlbumListAdapter.setListWatcher( this );
+
+		if(( mCurrentArtist != Constants.NULL_ID ) &&
+		   ( mAlbumList.size() == 0 )) {
+			mReceiver.setReceiver( this );
+
+			mNoiseData.GetAlbumList( mCurrentArtist, mReceiver );
+		}
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+
+		mAlbumListAdapter.setListWatcher( null );
+		mReceiver.clearReceiver();
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+
+		ButterKnife.reset( this );
 	}
 
 	@Override
@@ -201,19 +219,6 @@ public class AlbumListFragment extends Fragment
 		}
 		if( mAlbumListViewState != null ) {
 			outState.putParcelable( LIST_STATE, mAlbumListViewState );
-		}
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-
-		if( mAlbumListAdapter != null ) {
-			mAlbumListAdapter.setListWatcher( null );
-		}
-
-		if( mReceiver != null ) {
-			mReceiver.clearReceiver();
 		}
 	}
 
@@ -314,17 +319,31 @@ public class AlbumListFragment extends Fragment
 		mAlbumListAdapter.notifyDataSetChanged();
 	}
 
-	private class AlbumAdapter extends FilteredArrayAdapter<Album>
-							   implements Filterable, ListViewFilter.FilterClient<Album> {
+	protected class AlbumAdapter extends FilteredArrayAdapter<Album>
+							     implements Filterable, ListViewFilter.FilterClient<Album> {
 		private Context             mContext;
 		private LayoutInflater      mLayoutInflater;
 
-		private class ViewHolder {
-			public Button       PlayButton;
-			public TextView     TitleTextView;
-			public TextView     TrackCountTextView;
-			public TextView     PublishedTextView;
-			public TextView     PublishedHeaderTextView;
+		protected class ViewHolder {
+			public ViewHolder( View view ) {
+				ButterKnife.inject( this, view );
+			}
+
+			@InjectView( R.id.play_button )			    Button       PlayButton;
+			@InjectView( R.id.ali_title )			    TextView     TitleTextView;
+			@InjectView( R.id.ali_track_count)          TextView     TrackCountTextView;
+			@InjectView( R.id.ali_published )           TextView     PublishedTextView;
+			@InjectView( R.id.ali_published_header )    TextView     PublishedHeaderTextView;
+
+			@SuppressWarnings( "unused" )
+			@OnClick( R.id.play_button )
+			public void onPlayClick( View view ) {
+				Album   album = (Album)view.getTag();
+
+				if( album != null ) {
+					mEventBus.post( new EventPlayAlbum( album ));
+				}
+			}
 		}
 
 		public AlbumAdapter( Context context, ArrayList<Album> albumList ) {
@@ -355,23 +374,7 @@ public class AlbumListFragment extends Fragment
 				retValue = mLayoutInflater.inflate( R.layout.album_list_item, parent, false );
 
 				if( retValue != null ) {
-					views = new ViewHolder();
-					views.TitleTextView = (TextView)retValue.findViewById( R.id.ali_title );
-					views.TrackCountTextView = (TextView)retValue.findViewById( R.id.ali_track_count );
-					views.PublishedTextView = (TextView)retValue.findViewById( R.id.ali_published );
-					views.PublishedHeaderTextView = (TextView)retValue.findViewById( R.id.ali_published_header );
-					views.PlayButton = (Button)retValue.findViewById( R.id.play_button );
-
-					views.PlayButton.setOnClickListener( new View.OnClickListener() {
-						@Override
-						public void onClick( View view ) {
-							Album   album = (Album)view.getTag();
-
-							if( album != null ) {
-								EventBus.getDefault().post( new EventPlayAlbum( album ));
-							}
-						}
-					} );
+					views = new ViewHolder( retValue );
 
 					retValue.setTag( views );
 				}
